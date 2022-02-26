ae664c808017eb3221ae01e468821f15e2d1cad5
Kaarina <k1j20@tulleau.com>
2022-02-26 11:37:19 -0800

    Added styling to IncorrectTrivia page

--- src/components/IncorrectTrivia.js
+++ src/components/IncorrectTrivia.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link} from 'react-router-dom';
 
 export function IncorrectAnswer() {
-    console.log("incorrect");
     return (
         <main className="triviaBody centerMain">
             <div className="col-sm-12">
                 <div className="content">
-                    <h1>Incorrect answer!</h1>
-                    <p>Your answer was incorrect.</p>
+                    <h1 className="aboutHeader">Incorrect answer!</h1>
+                    <p className="aboutText">Your answer was incorrect. :(</p>
                     <Link to="/trivia2" className="btn btn-lg start centerElement">Try another question</Link>
                 </div>
             </div>
