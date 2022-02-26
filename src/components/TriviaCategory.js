319e35c3380c4cc90205464c456d92d831afa1ae
Kaarina <k1j20@tulleau.com>
2022-02-26 15:42:24 -0800

    added state variable for choosing category

--- src/components/TriviaCategory.js
+++ src/components/TriviaCategory.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link} from 'react-router-dom';
 
 export function TriviaCategory() {
+    const[category, setCategory] = useState('');
+
+    let chooseCat = (event) => {
+        setCategory(event.target.innerText);
+    }
+
+    let cat = "Urban Ecology";
+
     return (
         <main className="centerMain">
             <div className="container containerTrivia">
                 <div className="row">
                     <div className="col-md-4 d-flex centerElement">
                         <div class="buttonbody">
-                            <button type="button" className="btn btn-lg triviaCategory">Urban Ecology</button>
+                            <Link to="/triviaQuestion" onClick={chooseCat} value={cat} className="btn btn-lg triviaCategory">Urban Ecology</Link>
                         </div>
                     </div>
                     <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
-                            <button type="button" className="btn btn-lg triviaCategory buttonTan">Geographical Information Systems</button>
+                            <Link to="/triviaQuestion" onClick={chooseCat} className="btn btn-lg triviaCategory buttonTan">Geographical Information Systems</Link>
                         </div>
                     </div>
                     <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
-                            <Link to="/triviaQuestion" className="btn btn-lg triviaCategory">Climate Change</Link>
+                            <Link to="/triviaQuestion" onClick={chooseCat} className="btn btn-lg triviaCategory">Climate Change</Link>
                         </div>
                     </div>
                     <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
-                            <button type="button" className="btn btn-lg triviaCategory buttonTan">Wildlife Ecology</button>
+                            <Link to="/triviaQuestion" onClick={chooseCat} className="btn btn-lg triviaCategory buttonTan">Wildlife Ecology</Link>
                         </div>
                     </div>
                     <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
-                            <button type="button" className="btn btn-lg triviaCategory">Endangered Species</button>
+                            <Link to="/triviaQuestion" onClick={chooseCat} className="btn btn-lg triviaCategory">Endangered Species</Link>
                         </div>
                     </div>
                     <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
-                            <button type="button" className="btn btn-lg triviaCategory buttonTan">Random</button>
+                            <Link to="/triviaQuestion" onClick={chooseCat} className="btn btn-lg triviaCategory buttonTan">Random</Link>
                         </div>
                     </div>
                 </div>
