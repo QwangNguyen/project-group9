3d6ae484cf956d9f6ccb99188b7a25effce6bbfa
Kaarina <k1j20@tulleau.com>
2022-02-26 10:19:22 -0800

    Added routes to trivia pages to App

--- src/components/App.js
+++ src/components/App.js
@@ -20,12 +20,16 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="inputData" element={<InputData />} />
-        <Route path="insertData" element ={<InsertData />}>
+        <Route path="insertData" element={<InsertData />}>
           <Route path="insertEnergy" element={<InsertEnergy />} />
           <Route path="insertWaste" element={<InsertWaste />} />
           <Route path="insertVehicle" element={<InsertVehicle />} />
           <Route index element={<InsertVehicle/>} />
         </Route>
+        <Route path="trivia1" element={<Trivia1 />} />
+        <Route path="trivia2" element={<Trivia2 />} />
+        <Route path="trivia3" element={<Trivia3 />} />
+        <Route path="trivia4" element={<Trivia4 />} />
         <Route path="sustainabilityScore" element={<SustainabilityScore />}/>
         <Route path="dashboard" element={<Dashboard />}/>
       </Routes>
