56af71d60361c17bddcf6afd353eceef24573abe
Kaarina <k1j20@tulleau.com>
2022-02-28 19:48:29 -0800

    Removed unnecessary text

--- src/components/CorrectTrivia.js
+++ src/components/CorrectTrivia.js
@@ -17,16 +17,6 @@ export function CorrectTrivia(props) {
                                 <p className="message">You got it!</p>
                             </div>
                         </div>
-                        <div className="col-sm-12">
-                            <div className="content">
-                                <p className="explanation">Greenhouse gases may be a result of natural occurrence or human activity.
-                                    These gases include carbon dioxide (CO2), methane (CH4), water vapor, nitrous oxide (N2O)
-                                    and ozone (O3). Fluorinated gases are also considered to be greenhouse gases. Greenhouse
-                                    gases act like a heat-trapping blanket, making the Earth habitable for humans. However,
-                                    human activities have increased emissions of greenhouse gases into the atmosphere beyond
-                                    what the Earth can support, resulting in climate change.</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div className="container">
